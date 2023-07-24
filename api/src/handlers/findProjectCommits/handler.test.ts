9885d7d6da9fb06ff32a1fb38db52cbb0203dea5
Carlos Olivo <carlosengel011@gmail.com>
2023-07-24 17:21:04 -0400

    Add test for 'findProjectCommit' handler

--- api/src/handlers/findProjectCommits/handler.test.ts
+++ api/src/handlers/findProjectCommits/handler.test.ts
@@ -13,3 +13,33 @@ test('should be implemented', async () => {
         },
     })
 })
+
+test('should commit author be the same as username', async () => {
+    const res = await findProjectCommits({
+        url: PROJECT_URL,
+        username: USERNAME,
+        pagination: {
+            page: 1,
+            perPage: 1,
+        },
+    })
+
+    const commit = res.commits[0]
+
+    expect(commit.author.login).toBe(USERNAME)
+})
+
+test('should commit project url be the same as project url', async () => {
+    const res = await findProjectCommits({
+        url: PROJECT_URL,
+        username: USERNAME,
+        pagination: {
+            page: 1,
+            perPage: 1,
+        },
+    })
+
+    const commit = res.commits[0]
+
+    expect(commit.url).toBe(PROJECT_URL)
+})
