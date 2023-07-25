cacc2fee0824f1e96e1e6a28bc5b4065a31cfafa
Carlos Olivo <carlosengel011@gmail.com>
2023-07-25 16:02:44 -0400

    Add findProjectCommit endpoint

--- api/src/routes/commits.ts
+++ api/src/routes/commits.ts
@@ -1,5 +1,24 @@
 import { Hono } from 'hono'
+import findProjectCommitsRequestValidator from '../handlers/findProjectCommits/validator'
+import findProjectCommits, {
+    FindProjectCommitsRequest,
+} from '../handlers/findProjectCommits'
 
 const commitRouter = new Hono()
 
+commitRouter.get('/', findProjectCommitsRequestValidator, async (ctx) => {
+    const { url, branch, page, perPage } = ctx.req.valid('query')
+    const req: FindProjectCommitsRequest = {
+        url,
+        branch,
+        pagination: {
+            page,
+            perPage,
+        },
+    }
+
+    const res = await findProjectCommits(req)
+
+    return ctx.json(res)
+})
 export default commitRouter
