f3a4bebf238b01c5417c1203f67a846ccd7fee4a
Carlos Olivo <carlosengel011@gmail.com>
2023-07-25 14:46:30 -0400

    Remove test 'should commit project url be the same as project url'

--- api/src/handlers/findProjectCommits/handler.test.ts
+++ api/src/handlers/findProjectCommits/handler.test.ts
@@ -24,17 +24,3 @@ test('should commit author be the same as username', async () => {
 
     expect(commit.author.login).toBe(PROJECT_USERNAME)
 })
-
-test('should commit project url be the same as project url', async () => {
-    const res = await findProjectCommits({
-        url: PROJECT_URL,
-        pagination: {
-            page: 1,
-            perPage: 1,
-        },
-    })
-
-    const commit = res.commits[0]
-
-    expect(commit.url).toBe(PROJECT_URL)
-})
